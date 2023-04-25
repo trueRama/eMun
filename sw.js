4744462c23fb4d964c4fd7f2802596fa46a40a9d
trueRama <nkuutu.ramadhan@gmail.com>
2023-04-25 14:36:07 +0300

    manifest files updates

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-const CACHE = "pwabuilder-offline-page";
+const CACHE_NAME = 'cool-cache';
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "Offline.html";
+
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/uploads/'
+]
 
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") {
@@ -12,11 +16,12 @@ self.addEventListener("message", (event) => {
     }
 });
 
-self.addEventListener('install', async (event) => {
-    event.waitUntil(
-        caches.open(CACHE)
-            .then((cache) => cache.add(offlineFallbackPage))
-    );
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
 });
 
 if (workbox.navigationPreload.isSupported()) {
@@ -30,6 +35,28 @@ workbox.routing.registerRoute(
     })
 );
 
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+//pre cache
+// self.addEventListener('fetch', event => {
+//   event.respondWith(async () => {
+//     const cache = await caches.open(CACHE_NAME);
+//
+//     // match the request to our cache
+//     const cachedResponse = await cache.match(event.request);
+//
+//     // check if we got a valid response
+//     if (cachedResponse !== undefined) {
+//       // Cache hit, return the resource
+//       return cachedResponse;
+//     } else {
+//       // Otherwise, go to the network
+//       return fetch(event.request)
+//     };
+//   });
+// });
 self.addEventListener('fetch', (event) => {
     if (event.request.mode === 'navigate') {
         event.respondWith((async () => {
