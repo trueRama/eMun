351711c002324a6be2524f91d4608db9ad4165c7
Ramadhan Nkuutu <78150076+trueRama@users.noreply.github.com>
2024-12-01 08:31:01 +0300

    klo

--- includes/header.php
+++ includes/header.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>eMun App</title>
     <!-- endinject -->
-    <link rel="icon" href="uploads/logo.png">
+    <link rel="icon" href="/uploads/logo.png">
     <meta name="keywords" content="eMUN Get Healthy" />
-    <link rel="manifest" href="manifest.json">
-    <link rel="apple-touch-icon" href="uploads/logo192x192.png">
-    <link rel="apple-touch-icon" href="uploads/logo512x512.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="apple-touch-icon" href="/uploads/logo192x192.png">
+    <link rel="apple-touch-icon" href="/uploads/logo512x512.png">
     <meta name="apple-mobile-web-app-status-bar" content="#000000">
-    <script src="app.js"></script>
+    <script src="/app.js"></script>
     <?php
     include ("appStyle/app_css.php");
     ?>
